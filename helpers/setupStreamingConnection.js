2c024fa73f6ab7b6ebc49cd0c2f0c695385c425b
Ganesh Adapa <36997100+succulentpup@users.noreply.github.com>
2021-05-08 11:55:41 +0100

    Aggregated streamed data. Called the API to get minute pair ohlcv.

--- helpers/setupStreamingConnection.js
+++ helpers/setupStreamingConnection.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
+const got = require('got');
 
-const setupStreamingConnection = (apiKey) => {
+const setupStreamingConnection = (apiKey, streamedDataOut) => {
   const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
   ccStreamer.on('open', function open() {
     const subRequest = {
@@ -11,8 +12,42 @@ const setupStreamingConnection = (apiKey) => {
   });
 
   ccStreamer.on('message', function incoming(data) {
-    console.log(data);
+    const jsonFormatData = JSON.parse(data);
+    if (jsonFormatData.TYPE === '0'){
+      streamedDataOut.push(jsonFormatData);
+    }
   });
+
+  let aggregateOfThisMinute = 0;
+  let rtsMinute = 0;
+  setInterval(async () => {
+    aggregateOfThisMinute = 0;
+    let currentMinute = new Date().getMinutes();
+    streamedDataOut.forEach((dataPoint) => {
+      console.log({ dataPoint }, 'dataPoint');
+      rtsMinute = new Date(dataPoint.RTS * 1000).getMinutes();
+      if (rtsMinute === currentMinute) { // todo:  window to minute should be improved
+        aggregateOfThisMinute += dataPoint.Q;
+      }
+    });
+    console.log({ aggregateOfThisMinute }, 'aggregateOfThisMinute');
+
+    streamedDataOut = [];
+    // fetch the data using the API
+    const toTs = (Date.now()/1000).toFixed(0) - 1000;
+    const ohlcvurl = `https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&e=Coinbase&limit=60&toTs=${toTs}&api_key=${apiKey}`;
+
+    // todo:
+    // didn't get an opportunity to understand the input params of API well, so that not able to find a way to get aggregated result for the last minute00.
+    // could not find the param that can be compared with the aggregated value of computed thru streamConnection
+
+    const result =  await got(ohlcvurl)
+      .catch((error) => {
+        console.log(`Error fetching data from lastMinuteData: ${JSON.stringify(error, null, 1)}`);
+    });
+    const body = JSON.parse(result.body);
+    console.log(`body: ${JSON.stringify(body, null, 1)}`);
+  }, 60000);
 };
 
 module.exports = { setupStreamingConnection };
--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-const got = require('got');
-const chunk = require('lodash.chunk');
-
 const { setupStreamingConnection } = require('./helpers/setupStreamingConnection');
-const { safeJsonParse } = require('./helpers/isJson');
 
-const { MAX_BATCH_REQ_SIZE = 50 } = process.env;
 const { API_KEY = '4d6836b837f82283c0f87f472b846a529568eaf44805fdf3666693fa2dc36ff9' } = process.env;
 
-/**
- *
- * @desc Prints the fetched data on console
- * @param fetchedData
- */
-const printFetchedData = (fetchedData) =>
-  fetchedData.forEach((dataObj) => {
-    const url = Object.keys(dataObj)[0];
-    let data = dataObj[url];
-    console.log(`$url: ${url}: ${JSON.stringify(data, null, 1)} `);
-  });
-
-setupStreamingConnection(API_KEY);
-
-/**
- *
- * @desc  Expects a list of Urls and returns a promise that consists an array of json objects.
- *        This json objects are the fetched data from the given list of valid Urls
- * @param listOfUrls eg: ['https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json',
-                           'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json',]
- * @return {Promise<*[]>} eg: [ https://domain.com/abc.json: {key1: value1, ...},
- *                              https://domain1.com/xyz.json: {keyN: valueN,...}]
- *
- */
-// const fetchData = async (listOfUrls) => {
-//   console.log({ listOfUrls }, 'listOfUrls'); // debug log
-//   const validListOfHttpUrls = listOfUrls.filter(isValidHttpUrl);
-//   console.log({ validListOfHttpUrls }, 'validListOfHttpUrls'); // debug log
-//   const fetchedData = [];
-//   const chunkedHttpRequests = chunk(validListOfHttpUrls, MAX_BATCH_REQ_SIZE);
-//   for (const urlChunk of chunkedHttpRequests) {
-//     console.log({ urlChunk }, 'urlChunk');
-//     const firedRequests = urlChunk.map(
-//       (url) => got(url).catch((error) => {
-//         // console.log({ error }, 'error'); // debug log
-//         return error;
-//       }));
-//
-//     // avoid flood of http requests
-//     await Promise.all(firedRequests)
-//       .then((resultList) =>
-//         resultList.forEach(({ url, body }) => {
-//           const [err, jsonRes] = safeJsonParse(body);
-//           const InvalidResult = 'Invalid result. Result is not in json format';
-//           if (url) fetchedData.push({ [url]: err ? InvalidResult : jsonRes });
-//         }))
-//       .catch((error) => {
-//         console.log('Received Error while fetching data');
-//         console.log({ error }, 'error');
-//       });
-//   }
-//   // this is to manually test the fetched data
-//   printFetchedData(fetchedData);
-//   return fetchedData;
-// };
+const streamedData = [];
+setupStreamingConnection(API_KEY,streamedData);
 
 module.exports = {
   setupStreamingConnection,
-  // fetchData,
 };
 
 
