725eadc0946c5a51b07b40645ee5c126eaa6056a
Ganesh Adapa <36997100+succulentpup@users.noreply.github.com>
2021-05-08 11:59:19 +0100

    removed API key from source code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { setupStreamingConnection } = require('./helpers/setupStreamingConnection');
 
-const { API_KEY = '4d6836b837f82283c0f87f472b846a529568eaf44805fdf3666693fa2dc36ff9' } = process.env;
+const { API_KEY } = process.env;
 
 const streamedData = [];
 setupStreamingConnection(API_KEY,streamedData);
